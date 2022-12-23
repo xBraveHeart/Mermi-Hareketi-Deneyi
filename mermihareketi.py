52143f5aad94e82982e62f346036d8465e5a0f0b
Süleyman Latif Çifçi <117948084+slatifc@users.noreply.github.com>
2022-12-23 14:14:59 +0300

    Süleyman Latif Commit-1
    FPS ile ilgili ayarlamalar yaptım

--- mermihareketi.py
+++ mermihareketi.py
@@ -6,3 +6,5 @@ from math import *
 # OYUN PENCERESİNİN KURULUMU 
 DISPLAYSURF = pygame.display.set_mode((1280, 720), 0, 32)
 pygame.display.set_caption('mermi hareketi')
+FPS = 60 # frames per second setting
+fpsClock = pygame.time.Clock()
