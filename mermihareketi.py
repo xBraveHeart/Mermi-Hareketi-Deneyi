57c03268de393d4ce9f6c66678b4f08855f62a98
erenbarin <113824236+erenbarin@users.noreply.github.com>
2022-12-24 14:08:52 +0300

    Update mermihareketi.py

--- mermihareketi.py
+++ mermihareketi.py
@@ -5,7 +5,7 @@ from math import *
 
 # OYUN PENCERESİNİN KURULUMU 
 
-DISPLAYSURF = pygame.display.set_mode((1152, 648), 0, 32)
+EKRAN = pygame.display.set_mode((1152, 648), 0, 32)
 pygame.display.set_caption('mermi hareketi')
 
 # KARE/SANİYE AYARI
