83d844d6d7f8f6313750a92aac8dc1eec1c609f9
erenbarin <113824236+erenbarin@users.noreply.github.com>
2023-01-17 17:32:15 +0300

    emeği geçenler bölümü
    emeği geçenler bölümüne uygun yazıların yazılması

--- Giris.py
+++ Giris.py
@@ -15,17 +15,18 @@ def play():
     import mermihareketi
     mermihareketi.start()
 
-
-
-
-
-
-
-
-
-
 def options():
     while True:
         OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
 
         SCREEN.fill("#035b52")
+
+
+
+
+
+        AGA1_TEXT=get_font(20).render("EREN BARIN-032280032-github.com/erenbarin ", True, "White")
+        AGA1_RECT=AGA1_TEXT.get_rect(center=(576, 200))
+        SCREEN.blit(AGA1_TEXT,AGA1_RECT)
+
+
