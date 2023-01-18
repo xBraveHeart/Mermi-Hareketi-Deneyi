feedab0e01a3cc7e51e97cf86736d444abf60cb0
emirtutlu <115794145+emirtutlu@users.noreply.github.com>
2023-01-18 18:06:27 +0300

    Update button.py

--- button.py
+++ button.py
@@ -17,7 +17,10 @@ class Button():
 			screen.blit(self.image, self.rect)
 		screen.blit(self.text, self.text_rect)
 		
-		
+	def checkForInput(self, position):
+		if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
+			return True
+		return False		
 
 		
 		
