d89eb2bcc1c97ff41028690cc2faff0f2279eca2
Süleyman Latif Çifçi <117948084+slatifc@users.noreply.github.com>
2023-01-18 15:51:25 +0300

    Süleyman Latif Çifçi Commit

--- Giris.py
+++ Giris.py
@@ -92,3 +92,5 @@ def main_menu():
             
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
